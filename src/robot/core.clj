2cc74a2c4d42723fcb16b5096a24625f4097deb2
swapnull <martyn.e.rushton@gmail.com>
2016-02-29 23:46:25 +0000

    Have deliveryroutes working for manual destinations. Does not yet calculate fastest route

--- src/robot/core.clj
+++ src/robot/core.clj
@@ -12,55 +12,76 @@
 
 ;Load the file that contains all possible moves
 (load-file "src/robot/possibleMoves.clj")
+(load-file "src/robot/weights.clj")
 
-(def robotLocation ["r101"])
+(def robotLocation ["storage"])
+(def toCollect [""])
+(def carrying [""])
 
+;; HELPER FUNCTIONS
 (defn in?
  "true if collection contains element"
  [collection element]
  (some #(= element %) collection))
 
-(defn getPossibleMoves [start]
-  (get (first (filter #(= (first %) start) possibleMoves)) 1)
-)
-
-(defn getNewMoves [node path]
-  (filter #(not (in? path %)) (getPossibleMoves node))
-)
-
-(defn cost? 
-  "returns the cost of the path"
-  [path]
-  (count path)
-)
-
 ;; function taken from http://stackoverflow.com/a/35300641/2237577
-(defn flt [s] 
+(defn flt 
   "flatten to one sequence regardless of uneven nesting"
-  (mapcat #(if (every? coll? %) (flt %) (list %)) s)
-)
+  [s] 
+  (mapcat #(if (every? coll? %) (flt %) (list %)) s))
+
+(defn cost?
+  "Calculate the cost of a vertex"
+  [data]
+  (loop [w weights
+         c (first w)
+         t 0  
+         i 0] 
+    (if (empty? w)
+      (+ (- (count data) i) t)
+      (if (or (and (in? data (first c)) (in? data (second c))))
+        (recur (rest w) (first (rest w)) (+ t (last c)) (inc i))
+        (recur (rest w) (first (rest w)) t i)))))
+
+
+(defn getPossibleMoves 
+  "get possible moves from the current node"
+  [start]
+  (get (first (filter #(= (first %) start) possibleMoves)) 1))
+
+(defn getNewNodes 
+  "gets all nodes that are not in the path"
+  [node path]
+  (filter #(not (in? path %)) (getPossibleMoves node)))
 
 (defn getRoutes
+  "Get all the routes between two nodes"
   ([s t] (getRoutes s t [])) ;; only start and target provided
   ([s t p]
     (if(= s t)
      (conj p t)
-      (if (empty? (getNewMoves s p))
+      (if (empty? (getNewNodes s p))
           nil  ;; no moves available, return nil to signal this
-          (map #(flt (getRoutes % t (conj p s)))  (getNewMoves s (conj p s))) ;; recur for every option
-      )
-    )
-  )
-)
+          (flt (map #(flt (getRoutes % t (conj p s)))  (getNewNodes s (conj p s)))) ;; recur for every option
+      ))))
   
+(defn getShortestRoute 
+  "Get the shortest vertex between two nodes"
+  [start target]
+  (first (sort (zipmap (map #(min (cost? %)) (getRoutes start target)) (getRoutes start target)))))
 
+
+(defn deliveryRoute
+  [data]
+  (loop [d (conj data "office") r []]
+    (if (= 1 (count d))
+      r
+      (recur (rest d) (conj r (getShortestRoute (first d) (second d)))))))
 
 (defn -main
- "I don't do a whole lot ... yet."
+ "Wrapper to run program"
  [& args]
- (prn (flt (getRoutes (first robotLocation) "o131"))))
-
-
+ (prn (getShortestRoute (first robotLocation) "o131")))
 
 
 
