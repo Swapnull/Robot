8569d124637c3c170002f590f830c234be0bc7f0
swapnull <martyn.e.rushton@gmail.com>
2016-02-29 15:09:53 +0000

    getRoutes is working and returning sequences of sequences

--- src/robot/core.clj
+++ src/robot/core.clj
@@ -1,5 +1,5 @@
 (ns robot.core
-  (:gen-class))
+ (:gen-class))
 
 ;; door is twice as expensive as going through a door.
 ;; doors cannot stay open
@@ -9,35 +9,58 @@
 ;; diagonal moves are 1 & a bit if needed.
 ;; maybe use http://quil.info/ for GUI if everything is working early.
 
-;; TSP IMPLEMENTATIONS
-;; https://github.com/hdurer/clj-ga-salesman/blob/master/src/info/betareduction/dojo/holger_salesman.clj
-;; https://github.com/bgianfo/traveling-salesman/blob/master/clojure/tsp-ants.clj
-;; https://github.com/rrees/clj-ga-salesman/blob/master/src/salesman.clj
-
 
 ;Load the file that contains all possible moves
-(load-file "possibleMoves.clj")
+(load-file "src/robot/possibleMoves.clj")
 
-(def routes {
-'(a b) 2,
-'(b a) 3,
-'(c b) 4,
-'(b c) 4,
-'(c a) 5,
-'(a c) 2,
-'(d c) 9,
-'(c d) 7})
+(def robotLocation ["r101"])
 
-(defn targets[src]
-    (map (comp second key)
-         (filter
-             (fn [keyval] (= 'a (first (key keyval))))
-             routes)))
+(defn in?
+ "true if collection contains element"
+ [collection element]
+ (some #(= element %) collection))
 
-(defn score[route]
-    (get routes route))
+(defn getPossibleMoves [start]
+  (get (first (filter #(= (first %) start) possibleMoves)) 1)
+)
+
+(defn getNewMoves [node path]
+  (filter #(not (in? path %)) (getPossibleMoves node))
+)
+
+(defn cost? 
+  "returns the cost of the path"
+  [path]
+  (count path)
+)
+
+;; function taken from http://stackoverflow.com/a/35300641/2237577
+(defn flt [s] 
+  "flatten to one sequence regardless of uneven nesting"
+  (mapcat #(if (every? coll? %) (flt %) (list %)) s)
+)
+
+(defn getRoutes
+  ([s t] (getRoutes s t [])) ;; only start and target provided
+  ([s t p]
+    (if(= s t)
+     (conj p t)
+      (if (empty? (getNewMoves s p))
+          nil  ;; no moves available, return nil to signal this
+          (map #(flt (getRoutes % t (conj p s)))  (getNewMoves s (conj p s))) ;; recur for every option
+      )
+    )
+  )
+)
+  
+
 
 (defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (prn "hello world"))
+ "I don't do a whole lot ... yet."
+ [& args]
+ (prn (flt (getRoutes (first robotLocation) "o131"))))
+
+
+
+
+
